dab85991a6ce876f0f1a02ee936b53007d363eb2
TymoshchukD <tymoshchuk.dmytro@chnu.edu.ua>
2025-04-01 08:46:25 +0300

    Add files via upload

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+void removePositiveElements(std::vector<int>& arr) {
+    std::vector<int> result;
+    for (int num : arr) {
+        if (num <= 0) {
+            result.push_back(num);
+        }
+    }
+    arr = result;
+}
+
+int main() {
+    int N;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> N;
+
+    std::vector<int> A(N);
+    std::cout << "Enter " << N << " elements: ";
+    for (int i = 0; i < N; ++i) {
+        std::cin >> A[i];
+    }
+
+    removePositiveElements(A);
+
+    std::cout << "Array after removing positive elements: ";
+    for (int num : A) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
